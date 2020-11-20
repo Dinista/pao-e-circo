49aca456a5e28a18fc082fc9e6ed9279b364a170
Dinista <ra110752@uem.br>
2020-11-20 15:26:33 -0300

    Negociações atualização

---
+++ Felipe/recusar-troca.js
@@ -0,0 +1,36 @@
+const button = document.getElementById('btn-primary'); // or classname, whatever. it is your link or any node element instead of it. 
+const div = document.getElementById('linha2');
+const modelo = document.getElementsByClassName('modal-backdrop');
+const concretize = document.getElementsByClassName('btn-concretizar')[0];
+const divConcrete = document.getElementById('btn-concretiza-area');
+const ratingstar = document.getElementsByClassName('rate')[0];
+
+function addRow() {
+    const div = document.createElement('div');
+  
+    div.className = '';
+  
+    div.innerHTML = `
+    
+    `;
+  
+    document.getElementById('neg-pendentes').appendChild(div);
+}
+
+function toggleDiv(e) {
+    // e - event object - {button}.
+    e.preventDefault(); // only if you use <a> as node.
+    
+    div.style.display = 'none';
+    modelo[0].style.display = 'none';
+    addRow();
+}
+
+function hidediv(){
+    divConcrete.style.display = 'none';
+    ratingstar.style.display = 'block';
+}
+
+button.addEventListener('click', toggleDiv, false);
+
+concretize.addEventListener('click', hidediv, false);
