6db09cbd2e985d8d76d0e5a58a3761437c82ea3b
Dinista <ra110752@uem.br>
2020-12-07 01:36:20 -0300

    Login-Feito

--- Paginas/login.js
+++ Paginas/login.js
@@ -1,3 +1,19 @@
+const password = document.getElementById('inputpassaword');
+const email = document.getElementById('inputEmail');
+const formulario = document.getElementsByClassName('o-formulario2')[0];
+const erro = document.getElementById('erro');
+
+
+function autenticacao(){
+    if(email.value == "felipe@gmail.com"){
+        formulario.action = "indexLogado.html";
+    
+    }else{
+        formulario.action = "loginErro.html";   
+    }
+}
+
 function goBack() {
     window.history.back();
 }
+
