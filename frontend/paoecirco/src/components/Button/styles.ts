2828847c342097a29bee5ff57184b1e26b407719
evandroestima <evandro.fiorim@gmail.com>
2021-03-11 22:37:43 -0300

    gerenciar propaganda iniciado

--- frontend/paoecirco/src/components/Button/styles.ts
+++ frontend/paoecirco/src/components/Button/styles.ts
@@ -2,15 +2,16 @@ import styled from "styled-components";
 import { shade } from "polished";
 
 export const Container = styled.button`
-  background: #ff9000;
+  background: #fd5c7f;
   height: 56px;
   border: 0;
   color: #312e38;
-  font-weight: 500;
+  font-weight: bold;
+  font-size: 18px;
   margin-top: 16px;
   border-radius: 10px;
   padding: 0 16px;
-  width: 100%;
+  width: 80%;
   transition: background-color 0.2s;
 
   &:hover {
---
+++ frontend/paoecirco/src/components/PropagandaData/index.tsx
@@ -0,0 +1,32 @@
+export const PropagandaData = [
+  {
+    image:
+      "https://www.solucoesmidia.com.br/wp-content/uploads/2019/03/ruffles-1024x640.jpg",
+    EmpresaContratante: "Corsim & Cornão",
+    DataExpiracao: "12/05/2009",
+  },
+  {
+    image:
+      "https://www.solucoesmidia.com.br/wp-content/uploads/2019/03/ruffles-1024x640.jpg",
+    EmpresaContratante: "Corsim & Cornão",
+    DataExpiracao: "12/05/2009",
+  },
+  {
+    image:
+      "https://www.solucoesmidia.com.br/wp-content/uploads/2019/03/ruffles-1024x640.jpg",
+    EmpresaContratante: "Corsim & Cornão",
+    DataExpiracao: "12/05/2009",
+  },
+  {
+    image:
+      "https://www.solucoesmidia.com.br/wp-content/uploads/2019/03/ruffles-1024x640.jpg",
+    EmpresaContratante: "Corsim & Cornão",
+    DataExpiracao: "12/05/2009",
+  },
+  {
+    image:
+      "https://www.solucoesmidia.com.br/wp-content/uploads/2019/03/ruffles-1024x640.jpg",
+    EmpresaContratante: "Corsim & Cornão",
+    DataExpiracao: "12/05/2009",
+  },
+];
