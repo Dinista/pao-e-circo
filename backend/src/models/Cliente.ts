b0828e02f2e01362d50a73c3fbf877c0379e99fd
edu-fr <edu.felipe1@outlook.com>
2021-03-28 23:14:11 -0300

    hot-fix-erro-migrations-otm

--- backend/src/models/Cliente.ts
+++ backend/src/models/Cliente.ts
@@ -3,14 +3,19 @@ import {
   Entity,
   JoinColumn,
   ManyToOne,
+  OneToMany,
   PrimaryGeneratedColumn,
 } from "typeorm";
+import Anuncio from "./Anuncio";
 import Notificacoes from "./Notificacoes";
 
 @Entity("clientes")
 class Cliente {
   @PrimaryGeneratedColumn("uuid")
   id: string;
+  
+  @OneToMany(() => Anuncio, anuncio => anuncio.cliente)
+  anuncios: Anuncio[];
 
   @Column()
   name: string;
